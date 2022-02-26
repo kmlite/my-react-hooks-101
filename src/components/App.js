1ad1e32cc61cbd1300f9a59461f546e84639f01b
kmlite <k.m.lite3226@gmail.com>
2022-02-26 16:42:21 +0900

    implement event creation

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,40 @@
-import React from "react"
-import "bootstrap/dist/css/bootstrap.min.css";
+import React,{ useReducer, useState } from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import reducer from '../reducers/index'
+
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, [])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+
+  const addEvent = e => {
+    e.preventDefault()
+
+    dispatch({
+      type: 'CREATE_EVENT',
+      title,
+      body
+    })
+    setTitle('')
+    setBody('')
+  }
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
       <form>
         <div className="mb-2">
           <label htmlFor="formEventTitle" className="form-label">タイトル</label>
-          <input className="form-control" id="formEventTitle"></input>
+          <input type="text" className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)} />
         </div>
+        <div className="mb-2">
+            <label htmlFor="formEventBody" className="form-label">ボディ</label>
+            <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)} />
+        </div>
+        <button type="button" className="btn btn-primary mb-2" onClick={addEvent}>イベントを作成する</button>
+        <button type="button" className="btn btn-danger mb-2 ms-2">すべてのイベントを削除する</button>
       </form>
-      <div className="mb-2">
-          <label htmlFor="formEventBody" className="form-label">ボディ</label>
-          <textarea className="form-control" id="formEventBody"></textarea>
-      </div>
-      <button className="btn btn-primary mb-2" >イベントを作成する</button>
-      <button className="btn btn-danger mb-2 ms-2">すべてのイベントを削除する</button>
       <h4>イベント一覧</h4>
       <table className="table table-hover">
         <thead>
